097ae569341862378415febdc3374f8995677e35
keffri <keffri.w.neal@gmail.com>
2022-10-04 09:18:34 -0700

    change auth to user

--- controllers/authController.js
+++
@@ -1,4 +0,0 @@
-const User = require('../models/userModel');
-const passport = require('passport');
-const bcrypt = require('bcryptjs');
-const { body, validationResult } = require('express-validator');
