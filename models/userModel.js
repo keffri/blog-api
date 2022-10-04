df4145b686fe71da9a1409ee35dccc22d0d52583
keffri <keffri.w.neal@gmail.com>
2022-10-03 17:32:31 -0700

    updated isMember default

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: { type: String, minLength: 3, maxLength: 10, required: true },
   password: { type: String, minLength: 8, required: true },
-  isMember: { type: Boolean, default: false },
+  isMember: { type: Boolean, default: true },
   isAdmin: { type: Boolean, default: false },
 });
 
