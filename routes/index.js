5ea50b80c12194445a6c73f1bc4131784627d7db
keffri <keffri.w.neal@gmail.com>
2022-10-03 17:19:01 -0700

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Hello World!',
-    message: 'This is my Blog API project',
-  });
-});
+const post_controller = require('../controllers/postController');
+const comment_controller = require('../controllers/commentController');
+
+// POSTS
+router.get('/posts', post_controller.getPosts);
+router.post('/posts', post_controller.createPost);
+
+// SINGLE POST
+router.get('/posts/:post_id', post_controller.getPost);
+router.put('/posts/:post_id', post_controller.updatePost);
+router.delete('/posts/:post_id', post_controller.deletePost);
+
+// COMMENTS
+router.get('/posts/:post_id/comments', comment_controller.getComments);
+router.post('/posts/:post_id/comments', comment_controller.createComment);
+
+// SINGLE COMMENT
+router.get(
+  '/posts/:post_id/comments/:comment_id',
+  comment_controller.getComment
+);
+router.put(
+  '/posts/:post_id/comments/:comment_id',
+  comment_controller.updateComment
+);
+router.delete(
+  '/posts/:post_id/comments/:comment_id',
+  comment_controller.deleteComment
+);
 
 module.exports = router;
+
+// | GET /blog/posts - get all posts
+// | POST /blog/posts - create a post
+// | GET /blog/posts/:post_id - get a single post
+// | PUT  /blog/posts/:post_id - update a post
+// | DELETE /blog/posts/:post_id - delete a post
+
+// | GET /blog/posts/:post_id/comments - get all comments on single post
+// | POST /blog/posts/:post_id/comments - create a comment
+// | GET /blog/posts/:post_id/comments/:comment_id - get a single comment from post
+// | PUT /blog/posts/:post_id/comments/:comment_id - update a comment
+// | DELETE /blog/posts/:post_id/comments/:comment_id - delete a comment
