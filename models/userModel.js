b3b2b268f4e2e975a0403ccbe1fb28efed40fed2
keffri <keffri.w.neal@gmail.com>
2022-10-04 10:46:37 -0700

    removed member/admin

--- models/userModel.js
+++ models/userModel.js
@@ -4,8 +4,6 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: { type: String, minLength: 3, maxLength: 10, required: true },
   password: { type: String, minLength: 8, required: true },
-  isMember: { type: Boolean, default: true },
-  isAdmin: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', UserSchema);
