6acc2c136c9fd9af119846c0264a053ea924b5b9
keffri <keffri.w.neal@gmail.com>
2022-10-03 17:17:59 -0700

    updated user model

--- models/userModel.js
+++ models/userModel.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: { type: String, minLength: 3, maxLength: 10, required: true },
   password: { type: String, minLength: 8, required: true },
+  isMember: { type: Boolean, default: false },
+  isAdmin: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', UserSchema);
