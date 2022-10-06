19801b4942c688b1a98bbe917733e6afebfe9d06
keffri <keffri.w.neal@gmail.com>
2022-10-06 11:26:26 -0700

    post comment functionality

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,8 +1,26 @@
 const Comment = require('../models/commentModel');
 const Post = require('../models/postModel');
 
-exports.createComment = (req, res, next) => {};
+exports.postComment = async (req, res, next) => {
+  const post = await Post.findById(req.params.post_id).exec();
 
-exports.updateComment = (req, res, next) => {};
+  const comments = post.comments;
+
+  const comment = new Comment({
+    user: req.user.username,
+    comment: req.body.comment,
+  });
+
+  comments.push(comment);
+
+  Post.findByIdAndUpdate(req.params.post_id, { comments }, (err, post) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect(`/blog/posts/${req.params.post_id}`);
+  });
+};
+
+exports.putComment = (req, res, next) => {};
 
 exports.deleteComment = (req, res, next) => {};
