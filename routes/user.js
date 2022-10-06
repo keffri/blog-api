22b7e97d0bc3e1dbe74c314f7f1f28da966b8ee2
keffri <keffri.w.neal@gmail.com>
2022-10-06 08:39:05 -0700

    added logout

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,9 @@ const user_controller = require('../controllers/userController');
 router.get('/log-in', user_controller.getLogin);
 router.post('/log-in', user_controller.postLogin);
 
+// LOGOUT
+router.get('/log-out', user_controller.getLogout);
+
 // SIGN-UP
 router.get('/sign-up', user_controller.getSignup);
 router.post('/sign-up', user_controller.postSignup);
