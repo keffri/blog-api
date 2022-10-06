b97f84a2d0bd4ef6a9cfb7816786d37e97629408
keffri <keffri.w.neal@gmail.com>
2022-10-06 09:26:36 -0700

    get post functionality

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,8 +4,7 @@ exports.getPosts = (req, res, next) => {
   Post.find({}, 'title')
     .populate('post')
     .populate('date')
-    .populate('comments')
-    .sort({ date: 1 })
+    .sort({ date: -1 })
     .exec((err, posts_list) => {
       if (err) {
         return next(err);
@@ -13,6 +12,28 @@ exports.getPosts = (req, res, next) => {
 
       res.render('posts', {
         posts_list,
+      });
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  Post.findById(req.params.post_id)
+    .populate('title')
+    .populate('post')
+    .populate('date')
+    .populate('comments')
+    .exec((err, post) => {
+      if (err) {
+        return next(err);
+      }
+      if (post === null) {
+        const err = new Error('Post not found.');
+        err.status = 404;
+        return next(err);
+      }
+      res.render('post', {
+        title: post.title,
+        post,
       });
     });
 };
@@ -26,15 +47,6 @@ exports.createPost = (req, res, next) => {
       return next(err);
     }
     res.json({ message: 'post created' });
-  });
-};
-
-exports.getPost = (req, res, next) => {
-  Post.findById(req.params.post_id, (err, post) => {
-    if (err) {
-      return next(err);
-    }
-    res.json(post);
   });
 };
 
