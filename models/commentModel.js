cf21fba7a761c16f1d100a4298e638f03c692771
keffri <keffri.w.neal@gmail.com>
2022-10-06 10:39:20 -0700

    removed comment name

--- models/commentModel.js
+++ models/commentModel.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
   post: { type: Schema.Types.ObjectId, ref: 'Post' },
-  name: { type: String, minLength: 1, maxLength: 12, required: true },
   comment: { type: String, minLength: 10, maxLength: 140, required: true },
   date: { type: Date, default: Date.now, required: true },
 });
