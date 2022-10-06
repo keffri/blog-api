4f3b8bf44f2c554ac5bf9049c92f218f8a6465de
keffri <keffri.w.neal@gmail.com>
2022-10-06 10:43:36 -0700

    removed unncessary routes

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,11 +1,7 @@
 const Comment = require('../models/commentModel');
 const Post = require('../models/postModel');
 
-exports.getComments = (req, res, next) => {};
-
 exports.createComment = (req, res, next) => {};
-
-exports.getComment = (req, res, next) => {};
 
 exports.updateComment = (req, res, next) => {};
 
