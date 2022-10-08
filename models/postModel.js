99e8594013e9926d47b689a1c9d372932db698a5
keffri <keffri.w.neal@gmail.com>
2022-10-08 10:23:08 -0700

    changed comments type

--- models/postModel.js
+++ models/postModel.js
@@ -5,7 +5,7 @@ const PostSchema = new Schema({
   title: { type: String, minLength: 4, maxLength: 30, required: true },
   post: { type: String, minLength: 1, required: true },
   date: { type: Date, default: Date.now, required: true },
-  comments: { type: Array },
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
 });
 
 // create virtual date with luxon
