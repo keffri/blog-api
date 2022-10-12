992641e496f439da4087c95719496d92343b52b9
keffri <keffri.w.neal@gmail.com>
2022-10-12 11:46:13 -0700

    get/put comment error text rerender

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,29 +3,6 @@ const Post = require('../models/postModel');
 const { body, validationResult } = require('express-validator');
 const { DateTime } = require('luxon');
 
-// exports.postComment = async (req, res, next) => {
-//   const post = await Post.findById(req.params.post_id).exec();
-
-//   const comments = post.comments;
-
-//   const comment = new Comment({
-//     user: req.user.username,
-//     post: req.params.post_id,
-//     comment: req.body.comment,
-//   });
-
-//   comment.save();
-
-//   comments.push(comment);
-
-//   Post.findByIdAndUpdate(req.params.post_id, { comments }, (err, post) => {
-//     if (err) {
-//       return next(err);
-//     }
-//     res.redirect(`/blog/posts/${req.params.post_id}`);
-//   });
-// };
-
 exports.postComment = [
   body('comment')
     .trim()
@@ -41,9 +18,11 @@ exports.postComment = [
       .populate('comments')
       .exec();
 
+    let commentText = req.body.comment;
+
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.render('post', { post, errors: errors.array() });
+      return res.render('post', { post, commentText, errors: errors.array() });
     }
 
     const comments = post.comments;
@@ -84,27 +63,57 @@ exports.getComment = (req, res, next) => {
     });
 };
 
-exports.putComment = (req, res, next) => {
-  Comment.findById(req.params.comment_id, (err, comment) => {
-    if (err) {
-      return next(err);
+exports.putComment = [
+  body('comment')
+    .trim()
+    .isLength({ min: 4 })
+    .escape()
+    .withMessage('Comment is too short. (min: 4)')
+    .isLength({ max: 140 })
+    .escape()
+    .withMessage('Comment is too long. (max: 140)'),
+  async (req, res, next) => {
+    const comment = await Comment.findById(req.params.comment_id)
+      .populate('user')
+      .populate('comment')
+      .populate('date')
+      .exec();
+
+    let commentText = req.body.comment;
+    let postID = req.params.post_id;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.render('comment', {
+        comment,
+        commentText,
+        postID,
+        errors: errors.array(),
+      });
     }
 
-    comment.comment = req.body.comment;
-    comment.edited = true;
-    comment.editedDate = DateTime.fromJSDate(new Date()).toFormat(
-      'DDDD, h:mm:ss, a'
-    );
-
-    comment.save((err) => {
+    Comment.findById(req.params.comment_id, (err, comment) => {
       if (err) {
         return next(err);
       }
 
-      res.redirect(`/blog/posts/${req.params.post_id}`);
+      comment.comment = req.body.comment;
+      comment.edited = true;
+      comment.editedDate = DateTime.fromJSDate(new Date()).toFormat(
+        'DDDD, h:mm:ss, a'
+      );
+
+      comment.save((err) => {
+        if (err) {
+          return next(err);
+        }
+
+        res.redirect(`/blog/posts/${req.params.post_id}`);
+      });
     });
-  });
-};
+  },
+];
 
 exports.deleteComment = async (req, res, next) => {
   const post = await Post.findById(req.params.post_id).exec();
